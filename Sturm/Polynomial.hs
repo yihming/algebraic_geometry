90a78fdfcdab379a39dcf4a954ef94c52df1aa64
yihming <yang.yihming@gmail.com>
2013-06-12 23:18:17 -0600

    Finish the addition operation of polynomials in univariate case

--- Sturm/Polynomial.hs
+++ Sturm/Polynomial.hs
@@ -1,18 +1,35 @@
 module Polynomial where
 
-import Data.List
-import Data.Char
-
-data Polynomial = Uni [Double]
+data Polynomial = Poly [Double]
 
 instance Show Polynomial where
-show (Uni []) = "0"
-show (Uni nl@(n:ns)) = 
-  let degree = length nl - 1
-  in  case degree of
-        0 -> show n
-        _ -> show n ++ " x^" ++ show degree ++ " + "
-               ++ show (Uni ns)
+  show (Poly []) = ""
+  show (Poly xss@(x:xs)) = 
+    if length xss == 1
+       then " " ++ show x
+       else if x == 0     
+               then show (Poly xs)
+               else " " ++ show x ++ " x^" ++ show (length xss - 1) ++ " +" ++ show (Poly xs)
+    
+(<+>) :: Polynomial -> Polynomial -> Polynomial
+(Poly a) <+> (Poly b)
+  | length a > length b = 
+      let b' = (replicate (length a - length b) 0) ++ b
+      in  Poly (zipWith (+) a b')  
+  | length a < length b =
+      let a' = (replicate (length b - length a) 0) ++ a  
+      in  Poly (zipWith (+) a' b)    
+  | otherwise = Poly (zipWith (+) a b)
 
-test :: Polynomial
-test = [0, 1.5, 3, 0, 9]
+testpoly :: Polynomial
+testpoly = Poly [1.2, 3.0, 0, 0]
+
+(<*>) :: Polynomial -> Polynomial -> Polynomial
+(Poly a) <*> (Poly b) = undefined
+
+polydiv :: Polynomial -> Polynomial -> Polynomial
+(Poly a) `polydiv` (Poly b) = undefined
+
+
+infixl 6 <+>
+infixl 7 <*>

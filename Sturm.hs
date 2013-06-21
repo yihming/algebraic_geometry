a68838839b86506fc69aa47ab5585e1099879253
yihming <yang.yihming@gmail.com>
2013-06-21 14:00:29 -0600

    Implement Sturm-Tarski Theorem, but cannot pass the example

--- Sturm.hs
+++ Sturm.hs
@@ -6,18 +6,36 @@ import Polynomial
 test :: IO ()
 test = do
   let f = Poly [1,1,0,-1,-1]
-  let sturm_seq = genSturmSequence f
+  let f' = firstDerivative f
+  let sturm_seq = genSturmSequence f f'
   let a_lower = -5000
   let a_upper = 5000
   let num_roots = calcNumRoots sturm_seq (a_lower, a_upper)
   putStrLn $ "The number of real roots is " ++ show num_roots
 
-genSturmSequence :: Polynomial -> [Polynomial]
-genSturmSequence (Poly []) = error "Invalid polynomial!"
-genSturmSequence p@(Poly as) = 
-  let p0 = p
-      p1 = firstDerivative p0
-  in  [p0, p1] ++ (g p0 p1)
+test2 :: IO ()
+test2 = do
+  let f = Poly [1, 0, -1]
+  let g = Poly [1, 1]
+  let gs = [Poly [1], g, g <*> g]
+  let res_l = map (\x -> tarskiQuery x f (-5000, 5000)) gs
+  putStrLn $ show res_l
+
+
+tarskiQuery :: Polynomial -> Polynomial -> (Double, Double) -> Int
+tarskiQuery g f (a, b) = 
+  let sturm_seq = genSturmSequence f g
+      res_a = map (flip getValue a) sturm_seq
+      res_b = map (flip getValue b) sturm_seq
+      sc_a = calcSignChange res_a
+      sc_b = calcSignChange res_b
+  in  sc_a - sc_b
+
+genSturmSequence :: Polynomial -> Polynomial -> [Polynomial]
+genSturmSequence (Poly []) _ = error "Invalid polynomial!"
+genSturmSequence _ (Poly []) = error "Invalid polynomial!"
+genSturmSequence p0 p1 = 
+  [p0, p1] ++ (g p0 p1)
   where 
     g :: Polynomial -> Polynomial -> [Polynomial]
     g pa pb = 
@@ -53,3 +71,4 @@ calcSignChange as =
                                      then 1 + (calcSignChange (y:el))
                                      else calcSignChange (y:el)
                       else calcSignChange (y:el)
+

32dd345a24cc9a1ae1aac761e70558e885529ac3
yihming <yang.yihming@gmail.com>
2013-08-20 21:24:17 -0600

    Implement My MCCGB Algorithm

--- CGB/mccgb_example.s
+++ CGB/mccgb_example.s
@@ -1,4 +1,22 @@
 LIB "mccgb.s";
 
-ring r = (0, a, b, c), (x, y), lp;
+link out = "mccgb_example.mp";
+open(out);
+
+ring RingVar = (0, a, b, c), (u, v, x, y), lp;
+ring RingAll = 0, (u, v, x, y, a, b, c), lp  ;
+
+ideal polys = ax2y+a+3b2, abxy-acxy+abx+5c ;
+
+list aux = u, v;
+list vars = x, y;
+list params = a, b, c;
+
+list my_res;
+
+my_res = genMCCGB(polys, ideal(), list(), vars, params, aux, RingAll, RingVar, out);
+
+fprintf(out, "%s", string(my_res));
+
+close(out);
 

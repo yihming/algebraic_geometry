7947f9029324b1faf69b41c2d4854c8b5a3cd34f
yihming <yang.yihming@gmail.com>
2013-10-17 22:22:00 -0600

    Try update

--- MCCGB/sato_suzuki_3.s
+++ MCCGB/sato_suzuki_3.s
@@ -5,16 +5,10 @@ link out2 = "sato_suzuki_3_mccgb.mp";
 open(out);
 open(out2);
 
-ring RingVar = (0, a, b, c, d), (auxU, auxV, x(1), x(2), y(1), y(2), s), lp;
-
+ring r = (0, a, b, c, d), (x(1), x(2), y(1), y(2), s), lp;
+	
 ideal polys = a*x(1)^2 + b * y(1), c*y(2)^2 + d * x(2), (x(1) - x(2))^2 + (y(1) - y(2))^2 - s,
 	      4*a*c*x(1)*y(2) - b*d, 2*a*x(1)*y(1) - 2*a*x(1)*y(2) - b*x(1) + b*x(2);
-
-list aux = auxU, auxV;
-list vars = x(1), x(2), y(1), y(2), s;
-list params = a, b, c, d;
-
-list Auxiliary = aux;
 
 fprintf(out, "F = {");
 int i;
@@ -28,13 +22,13 @@ ideal G;
 list Modcgs;
 list mccgb;
 
-(mccgb, G, Modcgs) = genMCCGB(polys, ideal(), list(), vars, params, aux, RingVar, out2);
-
+(mccgb, G, Modcgs) = genMCCGB(polys, ideal(), list(), out2);
+	
 showMCCGB(mccgb, out);
-fprintf(out, "The size of CGB is: %s"+newline, string(size(G)));
-fprintf(out, "The size of MCCGB is: %s"+newline, string(size(mccgb)));
 fprintf(out, "%s" + newline, StringCGB(G));
 fprintf(out, "%s" + newline, StringModCGS_mod(Modcgs));
+fprintf(out, "The size of CGB is: %s"+newline, string(size(G)));
+fprintf(out, "The size of MCCGB is: %s"+newline, string(size(mccgb)));
 
 // Check the validity of my_res;
 string err_msg;

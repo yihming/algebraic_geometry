c38419cf803852ef270783d050e57efc5a951e9f
Yiming Yang <yang.yihming@gmail.com>
2014-08-04 16:01:26 -0600

    Add one example of a principal ideal

---
+++ MCCGB/principal.s
@@ -0,0 +1,35 @@
+// Example from A. Montes.
+LIB "simulation.lib";
+
+// The output file name.
+link out = "principal.mp";
+
+// 0 -- no intermediate debug information is printed to the output file;
+// >0 -- otherwise.
+int debug_mode = 0;
+
+// Times of running Algorithm 1 to generate different MCGBs.
+int sim_times = 15		;
+
+// sim_option = [opt1, opt2, opt3, opt4], where
+// opt1 -- 1 if running Algorithm 1; 0 otherwise.
+// opt2 -- 1 if running Algorithm 2; 0 otherwise.
+// opt3 -- 1 if running Algorithm 3; 0 otherwise.
+// opt4 -- 1 if running checking the CGBness of RGB; 0 otherwise.
+intvec sim_option = 0, 1, 1, 1, 0	;
+
+// degree reversed lex order.
+ring R = (0, Z, r, r2, d3, d4), (t), dp;
+
+ideal polys = (r^4-Z^3+d3^2)*t3 +
+	(d3^2*d4 - r2)*t2 +
+	(r^3*d3 - d4)*t +
+	(Z - d4^2)			;
+	
+ideal null_ideal = 0		;
+list nonnull_list = list()	;
+	
+simulate(null_ideal, nonnull_list, polys, sim_times, out, debug_mode, sim_option) ;
+//simulate_CGB(null_ideal, nonnull_list, polys, sim_times, out, debug_mode) ;
+//simulate_LeastMCGB(null_ideal, nonnull_list, polys, sim_times, out, debug_mode) ;
+//simulate_CCGB(null_ideal, nonnull_list, polys, sim_times, out, debug_mode) ;

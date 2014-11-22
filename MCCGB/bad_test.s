d1a1d8be84469d25060286d94f2cd16147018c8b
Yiming Yang <yang.yihming@gmail.com>
2014-11-22 11:35:33 -0700

    Correct the checkPairRec in mcgbcheck.lib

--- MCCGB/bad_test.s
+++ MCCGB/bad_test.s
@@ -1,14 +1,14 @@
 LIB "simulation.lib"		;
 
 // The output file name.
-link out = "bad_test_grlex.mp";
+link out = "bad_test_glex.mp";
 
 // 0 -- no intermediate debug information is printed to the output file;
 // >0 -- otherwise.
 int debug_mode = 0		;
 
 // Times of running Algorithm 1 to generate different MCGBs.
-int sim_times = 20		;
+	int sim_times = 1		;
 
 // sim_option = [opt1, opt2, opt3, opt4], where
 // opt1 -- 1 if running Algorithm 1; 0 otherwise.
@@ -17,7 +17,7 @@ int sim_times = 20		;
 // opt4 -- 1 if running checking the CGBness of RGB; 0 otherwise.
 intvec sim_option = 1,0,1,1,1	;
 	
-ring r = (0, a, b), (x, y, u, v), dp;
+ring r = (0, a, b), (x, y, u, v), Dp;
 
 ideal polys = (a-1)*u - b*(x-1), (a-1)*(x+1)+bu,
 	(a+1)*v - b*(y+1), (a+1)*(y-1) + bv ;
--- MCCGB/test_yxz.s
+++ MCCGB/test_yxz.s
@@ -2,7 +2,7 @@
 LIB "simulation.lib"		;
 
 // The output file name.
-link out = "test_zyx_grlex.mp";
+link out = "test_zyx_gradelex.mp";
 
 // 0 -- no intermediate debug information is printed to the output file;
 // >0 -- otherwise.
@@ -19,7 +19,7 @@ int sim_times = 20		;
 intvec sim_option = 1, 1, 1, 1, 0	;
 
 // lex order.
-ring r = (0, u, v), (y, x, z), dp;
+ring r = (0, u, v), (y, x, z), Dp;
 
 //ideal I = ux, uy, uz, vx, vy, vz;
 

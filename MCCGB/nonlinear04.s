813848d7f54dd726e953179154ca0478617ce398
yihming <yang.yihming@gmail.com>
2014-12-21 16:57:10 -0700

    Create completion.lib file for the completion algorithm

---
+++ MCCGB/nonlinear04.s
@@ -0,0 +1,45 @@
+// Example from A. Montes.
+LIB "simulation.lib";
+
+// The output file name.
+link out = "nonlinear04_glex.mp";
+
+// 0 -- no intermediate debug information is printed to the output file;
+// >0 -- otherwise.
+int debug_mode = 0;
+
+// Times of running Algorithm 1 to generate different MCGBs.
+int sim_times = 50	;
+
+// sim_option = [opt1, opt2, opt3, opt4], where
+// opt1 -- 1 if running Algorithm 1; 0 otherwise.
+// opt2 -- 1 if running Algorithm 2; 0 otherwise.
+// opt3 -- 1 if running Algorithm 3; 0 otherwise.
+// opt4 -- 1 if running checking the CGBness of RGB; 0 otherwise.
+intvec sim_option = 1, 1, 1, 1, 0	;
+
+// degree reversed lex order.
+ring R = (0, a, b), (x, y), Dp;
+	ideal I = (a-b)*x2, ay, a-b;
+
+	ideal J = a2x2, a2-b2 ;
+
+ideal polys = randomid(I, 2, 4) + randomid(J, 2, 4) ;
+	
+ideal null_ideal = 0		;
+	list nonnull_list = list()	;
+	simulate(null_ideal, nonnull_list, polys, sim_times, out, debug_mode, sim_option) ;
+
+	/*
+	int i				;
+	
+for (i = 1; i <= sim_times; i++) {
+	ideal polys = insertPolyToIdeal(randomid(I, 1, 4), randomid(J, 1, 4)[1]);
+	ideal null_ideal = 0		;
+list nonnull_list = list()	;
+
+}
+	*/
+//simulate_CGB(null_ideal, nonnull_list, polys, sim_times, out, debug_mode) ;
+//simulate_LeastMCGB(null_ideal, nonnull_list, polys, sim_times, out, debug_mode) ;
+//simulate_CCGB(null_ideal, nonnull_list, polys, sim_times, out, debug_mode) ;

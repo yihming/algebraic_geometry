be0af2f1ab41af8a8e2ebc63526f3b8c3be46cb4
Yiming Yang <yang.yihming@gmail.com>
2014-12-23 13:39:30 -0700

    Implement the new KSW algorithm of building MDBasis due to the faithful form

--- MCCGB/montes_examples.s
+++ MCCGB/montes_examples.s
@@ -210,25 +210,8 @@ simulate(null_ideal, nonnull_list, polys, sim_times, out, debug_mode, sim_option
 
 print("=========== s53 finished ==============" + newline) ;
 
-// s54.s:
-/*
-print("=========== s54 starts ==============") ;
-kill R, @R, @RP, @P, @RUA, @U, @AXU;
-ring R = (0, a, b, cv, cw, sv, sw), (x1, x2, y1, y2), lp;
-out = "s54_lex.mp"					;
-ideal polys = x1^2+y1^2-2*a*x1-2*y1,
-	x2^2+y2^2+2*b*x2-2*y2,
-	a*x1+y1+(a^2*cv-a^2+cv-1),
-	-b*x2+y2+(b^2*cw-b^2+cw-1),
-	-x1+a*y1+(a^2*sv+sv),
-	-x2-b*y2+(b^2*sw+sw),
-	-y1*x2+x1*y2-2*x1+2*x2;
-ideal null_ideal = 0		;
-list nonnull_list = a2+1, b2+1, a+b	;
-simulate(null_ideal, nonnull_list, polys, sim_times, out, debug_mode, sim_option) ;
 
-print("=========== s54 finished ==============" + newline) ;
-*/
+
 // s58.s:
 print("=========== s58 starts ==============") ;
 kill R, @R, @RP, @P, @RUA, @U, @AXU;
@@ -285,3 +268,22 @@ list nonnull_list = list()	;
 simulate(null_ideal, nonnull_list, polys, sim_times, out, debug_mode, sim_option) ;
 
 print("=========== sato_suzuki_3 finished ==============" + newline) ;
+
+// s54.s:
+
+print("=========== s54 starts ==============") ;
+kill R, @R, @RP, @P, @RUA, @U, @AXU;
+ring R = (0, a, b, cv, cw, sv, sw), (x1, x2, y1, y2), lp;
+out = "s54_lex.mp"					;
+ideal polys = x1^2+y1^2-2*a*x1-2*y1,
+	x2^2+y2^2+2*b*x2-2*y2,
+	a*x1+y1+(a^2*cv-a^2+cv-1),
+	-b*x2+y2+(b^2*cw-b^2+cw-1),
+	-x1+a*y1+(a^2*sv+sv),
+	-x2-b*y2+(b^2*sw+sw),
+	-y1*x2+x1*y2-2*x1+2*x2;
+ideal null_ideal = 0		;
+list nonnull_list = a2+1, b2+1, a+b	;
+simulate(null_ideal, nonnull_list, polys, sim_times, out, debug_mode, sim_option) ;
+
+print("=========== s54 finished ==============" + newline) ;

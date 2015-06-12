4558b1557236b1624be9838f65cf34f7a2a68750
Yiming Yang <yang.yihming@gmail.com>
2015-06-12 14:41:34 -0600

    add more examples

---
+++ MCCGB/constant3.s
@@ -0,0 +1,31 @@
+// Example 5.1 in D. Kapur, Y. Sun and D. Wang, "An Efficient Method for Computing Comprehensive Groebner Bases", ISSAC 2011.
+LIB "simulation.lib"		;
+
+// The output file name.
+link out = "constant3.mp";
+link dbg_out = "constant3.debug" ;
+link time_out = "constant3.time" ;
+	
+// 0 -- no intermediate debug information is printed to the output file;
+// >0 -- otherwise.
+int debug_mode = 0;
+
+// Times of running Algorithm 1 to generate different MCGBs.
+int sim_times = 20		;
+
+// sim_option = [opt1, opt2, opt3, opt4], where
+// opt1 -- 1 if running Algorithm 1; 0 otherwise.
+// opt2 -- 1 if running Algorithm 2; 0 otherwise.
+// opt3 -- 1 if running Algorithm 3; 0 otherwise.
+// opt4 -- 1 if running checking the CGBness of RGB; 0 otherwise.
+intvec sim_option = 1, 1, 1, 1, 0	;
+
+// lex order.
+ring r = (0, a, b, c), (x, y), Dp;
+
+ideal polys = c5-1, b3c-a3, a6-b6, by-a;
+ideal null_ideal = 0		;
+list nonnull_list = list()	;
+	
+simulate(null_ideal, nonnull_list, polys, sim_times, out, dbg_out, time_out, debug_mode, sim_option) ;
+//simulate_CGB(null_ideal, nonnull_list, polys, sim_times, out, debug_mode) ;
